3eea800482aed2d3f96439adbed93477dc13092d
bmwoolf <bradleymwoolf@gmail.com>
2025-07-26 10:15:51 -0700

    outline of app

---
+++ src/utils.py
@@ -0,0 +1,19 @@
+# Load appropriate BiGG GEM
+def load_bigg_model(host):
+    pass 
+
+# Modify model with edits
+def apply_gene_edits_to_model(model, gene_edits):
+    pass
+
+# Execute FBA optimization
+def run_fba_simulation(model, target_molecule):
+    pass
+
+# Get KEGG pathway data
+def parse_kegg_pathways(target_molecule):
+    pass
+
+# Create unified graph structure with NetworkX
+def build_metabolic_graph(bigg_model, kegg_data):
+    pass

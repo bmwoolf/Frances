b1bc32ad70235b81b42f68fe86d7137caea13284
Bradley Woolf <bradleymwoolf@gmail.com>
2025-07-28 11:47:01 -0700

    adding config for future-api build

---
+++ src/config.py
@@ -0,0 +1,8 @@
+# config file for inputs as preparation for turning this into an API
+config = {
+    "host": "E.coli",
+    "target": "limonene",
+    "constraints": {"max_knockouts": 3}
+}
+host = config["host"]
+target = config["target"]
